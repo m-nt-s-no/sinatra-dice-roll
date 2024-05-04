4247717fa8195e28b4d2c8adf52ae6240e187f6c
m-nt-s-no <45675504+m-nt-s-no@users.noreply.github.com>
2024-05-04 17:47:02 +0000

    added hidden Ruby tags

--- dice.rb
+++ dice.rb
@@ -13,42 +13,55 @@ get("/") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+    @rolls.push(die)
+  end
 	
   erb(:two_six)
 end
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    die = rand(1..10)
+    @rolls.push(die)
+  end
 
   erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die}."
-
+  1.times do
+    die = rand(1..20)
+    @rolls.push(die)
+  end
   erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first = rand(1..4)
-  second = rand(1..4)
-  third = rand(1..4)
-  fourth = rand(1..4)
-  fifth = rand(1..4)
-  sum = first + second + third + fourth + fifth
+  @rolls = []
 
-  @outcome = "You rolled a #{first}, a #{second}, a #{third}, a #{fourth}, and a #{fifth} for a total of #{sum}."
+  5.times do
+    die = rand(1..4)
+    @rolls.push(die)
+  end
 
   erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
